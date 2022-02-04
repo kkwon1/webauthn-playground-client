6942c751219df3292952787e97645e6cb4b3debf
Kevin Kwon <kevinkwon1995@gmail.com>
2022-02-03 22:25:13 -0800

    Initial working registration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [registered, setRegistered] = useState(false);
+  const [username, setUsername] = useState("");
+
+  const handleClick = () => {
+    if (username != "") {
+      axios
+        .post("http://localhost:8080/register", {
+          Username: username,
+        })
+        .then((response) => {
+          response.data.authenticatorSelection = {
+            requireResidentKey: false,
+            userVerification: "discouraged",
+          };
+          response.data.challenge = Uint8Array.from(response.data.challenge);
+          response.data.user.id = Uint8Array.from(response.data.user.id);
+          response.data.timeout = 60000;
+          return navigator.credentials.create({
+            publicKey: response.data,
+          });
+        })
+        .then((response) => {
+          console.log(response);
+          setRegistered(true);
+        });
+    } else {
+      console.log("Please enter valid username");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              name="username"
+            />
+          </label>
+        </form>
+        <button onClick={handleClick}>Register</button>
+        <div>
+          {registered ? `Username ${username} Registered Successfully!` : ""}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
